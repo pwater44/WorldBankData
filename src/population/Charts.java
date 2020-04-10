6065e2c627a45b27ceb0e0d16ce8309a474fc8d5
pwater44 <nvrithwik@hotmail.com>
2020-04-10 16:31:09 -0400

    Added line chart

--- src/population/Charts.java
+++ src/population/Charts.java
@@ -2,23 +2,24 @@ package population;
 
 import java.awt.Dimension;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class Charts extends JFrame{
-	public Charts(ArrayList<WorldBankEntry> data) {
-		PieDataset dataset = createDataset(data);
-		JFreeChart ch = ChartFactory.createPieChart3D("Populate", dataset);
+	
+	public Charts(WorldBankEntry data) {
+		XYSeriesCollection dataset = createDataset(data);
+		JFreeChart ch = ChartFactory.createXYLineChart(data.getCountry(), "Years", "Population", 
+				dataset, PlotOrientation.VERTICAL, true, true, false);
 		ChartPanel chartPanel = new ChartPanel(ch);
 		ch.removeLegend();
 		chartPanel.setPreferredSize(new Dimension(500, 300));
@@ -27,14 +28,18 @@ public class Charts extends JFrame{
 		setVisible(true);
 		pack();
 	}
-
-	private PieDataset createDataset(ArrayList<WorldBankEntry> data) {
-		DefaultPieDataset chartData = new DefaultPieDataset();
-		
-		for (WorldBankEntry k : data) {
-			
-			chartData.setValue(k.getCountry(), k.getToPopulation());
+	
+	private XYSeriesCollection createDataset(WorldBankEntry data) {
+		XYSeriesCollection chartData = new XYSeriesCollection();
+		XYSeries series = new XYSeries("Population");
+		ArrayList<Long> popLst = data.getPopulation();
+		int i=0;
+		for (Long k : popLst) {
+			int year = 1960 + i;
+			i++;
+			series.add(year, k);
 		}
+		chartData.addSeries(series);
 		return chartData;
 	}
 }
